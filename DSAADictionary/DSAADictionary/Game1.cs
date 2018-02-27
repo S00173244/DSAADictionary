976e3ff021dc95c2b16b956365723c60c21009c0
S00173244 <nels.puthoor@mail.itsligo.ie>
2018-02-27 11:53:39 +0000

    Need to work on few things

--- DSAADictionary/DSAADictionary/Game1.cs
+++ DSAADictionary/DSAADictionary/Game1.cs
@@ -2,6 +2,7 @@
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 using NSLoader;
+using System;
 using System.Collections.Generic;
 
 namespace DSAADictionary
@@ -13,10 +14,11 @@ namespace DSAADictionary
     {
         GraphicsDeviceManager graphics;
         SpriteBatch spriteBatch;
-
+        TimeSpan time = new TimeSpan(); 
         Dictionary<string, Texture2D> badges = new Dictionary<string, Texture2D>();
         KeyValuePair<string,Texture2D> _current;
 
+        
         public Game1()
         {
             graphics = new GraphicsDeviceManager(this);
@@ -45,6 +47,7 @@ namespace DSAADictionary
             // Create a new SpriteBatch, which can be used to draw textures.
             spriteBatch = new SpriteBatch(GraphicsDevice);
             badges = Loader.ContentLoad<Texture2D>(Content,"Badges");
+            badges.GetEnumerator().MoveNext();
             // TODO: use this.Content to load your game content here
         }
 
@@ -66,7 +69,23 @@ namespace DSAADictionary
         {
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
+            time += gameTime.ElapsedGameTime;
+            if(time.Seconds > 1)
+            {
+                time = new TimeSpan();
+                badges.GetEnumerator().MoveNext();
+                _current = badges.GetEnumerator().Current;
+            }
+            
+            // _current = badges.;
 
+            //badges.GetEnumerator().MoveNext();
+            //Console.WriteLine(badges.GetEnumerator().); 
+            //time.Add(gameTime.ElapsedGameTime);
+
+            //Console.WriteLine("Time : " + time);
+            //Console.WriteLine("Enum : " + badges.GetEnumerator());
+            //Console.WriteLine("Game time : " + gameTime.ElapsedGameTime);
             // TODO: Add your update logic here
 
             base.Update(gameTime);
@@ -79,7 +98,11 @@ namespace DSAADictionary
         protected override void Draw(GameTime gameTime)
         {
             GraphicsDevice.Clear(Color.CornflowerBlue);
-
+            
+            spriteBatch.Begin();
+            spriteBatch.Draw(_current.Value, Vector2.Zero, Color.White);
+           
+            spriteBatch.End();
             // TODO: Add your drawing code here
 
             base.Draw(gameTime);
